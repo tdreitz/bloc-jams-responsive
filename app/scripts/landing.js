8b28b7bb4615e731d346ebee48ceb4d6836685b1
Tyler Reitz <tdreitz@gmail.com>
2015-10-15 14:24:36 -0700

    assgn 22 completed, refactored DOM animation to single function w/ loop

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -4,30 +4,15 @@ var animatePoints = function() {
   
   var points = document.getElementsByClassName('point');
 
-  console.log(points);
-
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.tranform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+  var revealPoint = function() {
+    for (var i = 0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+      points[i].style.tranform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
   };
+  
+  revealPoint();
 
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
 };
