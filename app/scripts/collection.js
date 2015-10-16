fa841cd7ea6c0c5814bce45d55f98a16be86b0eb
Tyler Reitz <tdreitz@gmail.com>
2015-10-15 21:47:13 -0700

    cp 24 DOM scripting collection.html completed, creates album cover dynamically

---
+++ app/scripts/collection.js
@@ -0,0 +1,27 @@
+console.log("collection.js loaded!");
+
+var collectionItemTemplate = 
+  '<div class="collection-album-container column fourth">'
++ '  <img src="../assets/images/album_covers/01.png" alt="plchldr">'
++ '  <div class="collection-album-info caption">'
++ '    <p>'
++ '      <a class="album-name" href="album.html">The Colors</a>'
++ '      <br/>'
++ '      <a href="album.html">Pablo Picasso</a>'
++ '      <br/>'
++ '      X songs'
++ '      <br/>'
++ '    </p>'
++ '  </div>'
++ '</div>'
+;
+
+window.onload = function() {
+  var collectionContainer = document.getElementsByClassName("album-covers")[0];
+  collectionContainer.innerHTML = " ";
+  for (var i = 0; i < 12; i++) {
+    collectionContainer.innerHTML += collectionItemTemplate;
+  }
+}
+
+console.log("collection.js has run");
